ddde00a2bd70347c96c13c307baeff6899ad3af7
Zebulun Arendsee <zbwrnz@gmail.com>
2021-10-22 18:05:45 -0500

    Add definitions for paramorphisms
    Borrowing heavily from Patrick Thomson

--- src/Schema.hs
+++ src/Schema.hs
@@ -9,18 +9,34 @@ module Schema
  , bottomUp
  ) where
 
-import Control.Arrow ((>>>), (<<<))
+import Control.Arrow ((>>>), (&&&), (<<<))
+-- (&&&) :: (a -> b) -> (a -> c) -> (b, c)
+
+
+(&) :: a -> (a -> b) -> b
+(&) = flip ($)
 
 newtype Term f = In { out :: f (Term f) }
 
 type Algebra f a = f a -> a
 type Coalgebra f a = a -> f a
+type RAlgebra f a = f (Term f, a) -> a
+type RAlgebra' f a = Term f -> f a -> a
 
 cata :: Functor f => Algebra f a -> Term f -> a
 cata fn = out >>> fmap (cata fn) >>> fn
 
 ana :: Functor f => Coalgebra f a -> a -> Term f
 ana fn = In <<< fmap (ana fn) <<< fn
+
+para :: Functor f => RAlgebra f a -> Term f -> a
+para fn = out >>> fmap (id &&& para fn) >>> fn
+
+para' :: Functor f => RAlgebra' f a -> Term f -> a
+para' fn t = out t & fmap (para' fn) & fn t
+
+cata' :: Functor f => Algebra f a -> Term f -> a
+cata' f = para' (const f)
 
 bottomUp :: Functor f => (Term f -> Term f) -> Term f -> Term f
 bottomUp fn = cata (In >>> fn)
