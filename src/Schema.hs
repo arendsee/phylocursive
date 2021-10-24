977bf23625af143d135a63a20c3cda3da72e9ff2
Zebulun Arendsee <zbwrnz@gmail.com>
2021-10-24 15:48:20 -0500

    Added a completely copied implementation of histo
    Again, it is just code taken from Patrick Thomson (thanks Patrick).
    Histomorphisms are unbelievably cool, they seem to be a way to
    accomplish dynamic programming efficiently using recursion. I should
    implement a parsimony tree building algorithm. And, though not strictly
    phylogenetics, an alignment algorithm.

--- src/Schema.hs
+++ src/Schema.hs
@@ -9,8 +9,9 @@ module Schema
  , bottomUp
  ) where
 
-import Control.Arrow ((>>>), (&&&), (<<<))
+import Control.Arrow ((>>>), (&&&), (|||), (<<<))
 -- (&&&) :: (a -> b) -> (a -> c) -> (b, c)
+-- (|||) :: a b d -> a c d -> a (Either b c) d
 
 
 (&) :: a -> (a -> b) -> b
@@ -23,6 +24,11 @@ type Coalgebra f a = a -> f a
 type RAlgebra f a = f (Term f, a) -> a
 type RAlgebra' f a = Term f -> f a -> a
 
+-- the duel of an Ralgebra must reverse both the function arrow AND the concept
+-- of a tuple, the reverse of a tuple's tying of things together (product) is
+-- an Either's holding of things apart (a sum).
+type RCoalgebra f a = a -> f (Either (Term f) a)
+
 cata :: Functor f => Algebra f a -> Term f -> a
 cata fn = out >>> fmap (cata fn) >>> fn
 
@@ -31,6 +37,23 @@ ana fn = In <<< fmap (ana fn) <<< fn
 
 para :: Functor f => RAlgebra f a -> Term f -> a
 para fn = out >>> fmap (id &&& para fn) >>> fn
+
+apo :: Functor f => RCoalgebra f a -> a -> Term f
+apo f = In <<< fmap (id ||| apo f) <<< f
+
+
+data Attr f a = Attr
+  { attribute :: a
+  , hole :: f (Attr f a)
+  }
+
+type CVAlgebra f a = f (Attr f a) -> a
+
+histo :: Functor f => CVAlgebra f a -> Term f -> a
+histo h = worker >>> attribute where
+  worker = out >>> fmap worker >>> (h &&& id) >>> mkAttr
+  mkAttr (a, b) = Attr a b
+
 
 para' :: Functor f => RAlgebra' f a -> Term f -> a
 para' fn t = out t & fmap (para' fn) & fn t
